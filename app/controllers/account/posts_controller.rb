b98e6edaa47c3eb4c98c0e32be054bf7780c12ff
liujia <ericrunner123456@yahoo.com>
2017-03-10 06:49:14 +0800

    implemnt edit & destroy function on post

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -1,9 +1,27 @@
 class Account::PostsController < ApplicationController
-
 before_action :authenticate_user!
-
 def index
-  @posts = current_user.posts
-end 
+  @posts = current_user.posts.recent
+end
+def edit
+  @post = Post.find(params[:id])
+end
+def update
+  @post = Post.find(params[:id])
+  if @post.update(post_params)
+    redirect_to account_posts_path
+  end
+end
 
+
+def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+  redirect_to account_posts_path
+end
+
+private
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
